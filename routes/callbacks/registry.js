2ebffdd726bd2b27bd5911ab12ae5494ffbba232
Eltee <l361an@gmail.com>
2026-03-06 14:23:12 +0700

    Create registry.js

---
+++ routes/callbacks/registry.js
@@ -0,0 +1,28 @@
+// routes/callbacks/registry.js
+import { buildOfficerExact } from "./officer.js";
+import { buildPartnerToolsExact } from "./partnerTools.js";
+import { buildPartnerDatabaseHandlers } from "./partnerDatabase.js";
+import { buildPartnerModerationHandlers } from "./partnerModeration.js";
+import { buildSuperadminHandlers } from "./superadmin.js";
+import { buildVerificationHandlers } from "./verification.js";
+
+function mergeHandlers(...parts) {
+  const EXACT = {};
+  const PREFIX = [];
+  for (const p of parts) {
+    if (p?.EXACT) Object.assign(EXACT, p.EXACT);
+    if (p?.PREFIX?.length) PREFIX.push(...p.PREFIX);
+  }
+  return { EXACT, PREFIX };
+}
+
+export function createHandlers() {
+  return mergeHandlers(
+    buildOfficerExact(),
+    buildPartnerToolsExact(),
+    buildPartnerDatabaseHandlers(),
+    buildPartnerModerationHandlers(),
+    buildSuperadminHandlers(),
+    buildVerificationHandlers()
+  );
+}
