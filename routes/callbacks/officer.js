376e4c02d250e27ec79aaecba93f21a647c53042
Eltee <l361an@gmail.com>
2026-03-07 10:10:00 +0700

    Update officer.js

--- routes/callbacks/officer.js
+++ routes/callbacks/officer.js
@@ -2,17 +2,23 @@
 import { sendMessage, editMessageReplyMarkup } from "../../services/telegramApi.js";
 import { isAdminRole } from "../../utils/roles.js";
 import { buildOfficerHomeKeyboard } from "./keyboards.js";
+import { CALLBACKS } from "../telegram.constants.js";
+import { buildOfficerHomeText } from "../telegram.messages.js";
 
 export function buildOfficerExact() {
   const EXACT = {};
 
-  EXACT["officer:home"] = async (ctx) => {
+  EXACT[CALLBACKS.OFFICER_HOME] = async (ctx) => {
     const { env, role, adminId, msgChatId, msgId } = ctx;
     if (!isAdminRole(role)) return true;
 
-    if (msgChatId && msgId) await editMessageReplyMarkup(env, msgChatId, msgId, null).catch(() => {});
-    const text = "Hallo Officer TeMan...\nSilahkan tekan tombol dibawah atau ketik /help untuk bantuan.";
-    await sendMessage(env, adminId, text, { reply_markup: buildOfficerHomeKeyboard(role) });
+    if (msgChatId && msgId) {
+      await editMessageReplyMarkup(env, msgChatId, msgId, null).catch(() => {});
+    }
+
+    await sendMessage(env, adminId, buildOfficerHomeText(), {
+      reply_markup: buildOfficerHomeKeyboard(role),
+    });
     return true;
   };
 
