f6beb594b9d8cf1a6db93e16d236fb28d62a1f7c
Eltee <l361an@gmail.com>
2026-03-07 10:10:18 +0700

    Update partnerTools.js

--- routes/callbacks/partnerTools.js
+++ routes/callbacks/partnerTools.js
@@ -2,15 +2,18 @@
 import { sendMessage, editMessageReplyMarkup } from "../../services/telegramApi.js";
 import { clearSession } from "../../utils/session.js";
 import { buildPartnerToolsKeyboard } from "./keyboards.js";
+import { CALLBACKS } from "../telegram.constants.js";
 
 export function buildPartnerToolsExact() {
   const EXACT = {};
 
-  EXACT["pt:menu"] = async (ctx) => {
+  EXACT[CALLBACKS.PARTNER_TOOLS_MENU] = async (ctx) => {
     const { env, adminId, msgChatId, msgId } = ctx;
 
     await clearSession(env, `state:${adminId}`).catch(() => {});
-    if (msgChatId && msgId) await editMessageReplyMarkup(env, msgChatId, msgId, null).catch(() => {});
+    if (msgChatId && msgId) {
+      await editMessageReplyMarkup(env, msgChatId, msgId, null).catch(() => {});
+    }
 
     await sendMessage(env, adminId, "🧰 <b>Partner Tools</b>\nPilih menu:", {
       parse_mode: "HTML",
